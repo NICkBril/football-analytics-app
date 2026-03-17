3d0a82dce99c211767afe0fa167885e641ae9ecd
Nick Bril <NickBril.ua@gmail.com>
2026-03-17 20:09:20 +0200

    update MatchesPage footer: add contact info and style placeholders

--- client/src/pages/MatchesPage.jsx
+++ client/src/pages/MatchesPage.jsx
@@ -36,7 +36,13 @@ function MatchesPage() {
         </div>
       ))}
 
-      <footer>Football Analytics App © 2026</footer>
+      <footer>
+        Football Analytics App © 2026
+        <div className="contact-info">
+            Email: <a href="mailto:NickBril.ua@gmail.com">NickBril.ua@gmail.com</a> | 
+            Telegram: <a href="https://t.me/barankaY">@NickBril</a>
+        </div>
+      </footer>
     </div>
   );
 }
