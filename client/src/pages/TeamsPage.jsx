7813417c829126d21ff1b0024640287d6abde61f
Nick Bril <NickBril.ua@gmail.com>
2026-03-18 16:56:50 +0200

    add footer to TeamsPage

--- client/src/pages/TeamsPage.jsx
+++ client/src/pages/TeamsPage.jsx
@@ -56,6 +56,14 @@ function TeamsPage() {
           </div>
         </div>
       ))}
+
+      <footer>
+        Football Analytics App © 2026
+        <div className="contact-info">
+          Email: <a href="mailto:NickBril.ua@gmail.com">NickBril.ua@gmail.com</a> | 
+          Telegram: <a href="https://t.me/barankaY">@NickBril</a>
+        </div>
+      </footer>
     </div>
   );
 }
