af78d5833c35376b0bd569225a5970c5b421fbef
Nick Bril <NickBril.ua@gmail.com>
2026-03-18 17:11:57 +0200

    create Footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer>
+      Football Analytics App © 2026
+      <div className="contact-info">
+        Email: <a href="mailto:NickBril.ua@gmail.com">NickBril.ua@gmail.com</a> | 
+        Telegram: <a href="https://t.me/barankaY">@NickBril</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
