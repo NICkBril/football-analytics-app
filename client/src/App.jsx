ae6322c3002648d4fcc576f84fe05935ed563a4b
Nick Bril <NickBril.ua@gmail.com>
2026-03-23 13:08:55 +0200

    Add TablePage to application routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import TeamsPage from "./pages/TeamsPage";
 import MatchesPage from "./pages/MatchesPage";
 import HomePage from "./pages/HomePage";
 import Footer from "./components/Footer";
+import TablePage from "./pages/TablePage";
 
 function App() {
   const [page, setPage] = useState(() => {
@@ -20,12 +21,14 @@ function App() {
         <button onClick={() => setPage("home")}>Home</button>
         <button onClick={() => setPage("teams")}>Teams</button>
         <button onClick={() => setPage("matches")}>Matches</button>
+        <button onClick={() => setPage("table")}>Table</button>
       </nav>
 
       <div className="content">
         {page === "home" && <HomePage />}
         {page === "teams" && <TeamsPage />}
         {page === "matches" && <MatchesPage />}
+        {page === "table" && <TablePage />}
       </div>
 
       <Footer />
