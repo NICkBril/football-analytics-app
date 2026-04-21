d98329dee8b7e7139e78ae4f7a0c0cc74c1549dc
Nick Bril <NickBril.ua@gmail.com>
2026-04-22 01:35:50 +0300

    Replace flat block with structured per-type layout

--- client/src/components/Skeleton.jsx
+++ client/src/components/Skeleton.jsx
@@ -1,14 +1,65 @@
 import "../styles/Skeleton.css";
 
 function Skeleton({ type }) {
-  const classes = `skeleton ${type}`;
+  if (type === "card") {
+    return (
+      <div className="skeleton-card">
+        <div className="skeleton-card-left">
+          <div className="skeleton-block skeleton-circle" />
+        </div>
+        <div className="skeleton-card-body">
+          <div className="skeleton-block skeleton-line w-60" />
+          <div className="skeleton-block skeleton-line w-40" />
+        </div>
+        <div className="skeleton-shimmer" />
+      </div>
+    );
+  }
 
-  return <div className={`skeleton-wrapper ${type}`}>
-    <div className={classes}></div>
-    <div className="shimmer-wrapper">
-      <div className="shimmer"></div>
+  if (type === "title") {
+    return (
+      <div className="skeleton-title-wrap">
+        <div className="skeleton-block skeleton-line w-50" style={{ height: "22px" }} />
+        <div className="skeleton-shimmer" />
+      </div>
+    );
+  }
+
+  if (type === "text") {
+    return (
+      <div className="skeleton-text-wrap">
+        <div className="skeleton-block skeleton-line w-100" />
+        <div className="skeleton-block skeleton-line w-80" />
+        <div className="skeleton-block skeleton-line w-60" />
+        <div className="skeleton-shimmer" />
+      </div>
+    );
+  }
+
+  if (type === "avatar") {
+    return (
+      <div className="skeleton-avatar-wrap">
+        <div className="skeleton-block skeleton-circle skeleton-circle-lg" />
+        <div className="skeleton-shimmer" />
+      </div>
+    );
+  }
+
+  if (type === "thumbnail") {
+    return (
+      <div className="skeleton-thumbnail-wrap">
+        <div className="skeleton-block skeleton-rect" />
+        <div className="skeleton-shimmer" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="skeleton-title-wrap">
+      <div className="skeleton-block skeleton-line w-100" />
+      <div className="skeleton-shimmer" />
     </div>
-  </div>;
+  );
 }
 
 export default Skeleton;
